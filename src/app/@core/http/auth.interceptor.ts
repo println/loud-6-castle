ed25736a3b7bdff975c15323f4d290d0b72b3912
Felipe Santos <live.proto@hotmail.com>
2020-07-11 18:36:24 -0300

    Adicionando interceptor para envio do token jwt

---
+++ src/app/@core/http/auth.interceptor.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+
+const TOKEN_HEADER_KEY = 'Authorization';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    let authReq = req;
+    const token = '';
+    if (token != null) {
+      authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
+    }
+    return next.handle(authReq);
+  }
+}
