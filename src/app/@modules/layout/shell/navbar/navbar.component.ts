e2f9000922023e6cad92dcf1159d79bcf7a19fcd
println <live.proto@hotmail.com>
2021-11-02 15:38:32 -0300

    Add dropdown mark to navbar

--- src/app/@modules/layout/shell/navbar/navbar.component.ts
+++ src/app/@modules/layout/shell/navbar/navbar.component.ts
@@ -1,13 +1,35 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostBinding, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-navbar, [app-navbar]',
   templateUrl: './navbar.component.html',
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, AfterViewInit {
   @HostBinding('class') class = 'c-navbar';
 
-  constructor() {}
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.markDropdownItens();
+  }
+
+  private markDropdownItens() {
+    const parentSelector = `.${this.class}__item`;
+    const childSelector = `.${this.class}__nav`;
+    const dropdownClass = `${this.class}__item--dropping`;
+
+    const parents = this.querySelectorHas(parentSelector, childSelector);
+    parents.forEach((el) => {
+      this.renderer.addClass(el, dropdownClass);
+    });
+  }
+
+  private querySelectorHas(parentSelector: string, childSelector: string): HTMLElement[] {
+    const navbarEl = this.elementRef.nativeElement;
+    return [].filter.call(navbarEl.querySelectorAll(parentSelector), (elem: HTMLElement) => {
+      return elem.querySelector(childSelector);
+    });
+  }
 }
