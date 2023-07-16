a13e93da21e6bf598b0b6d40159c78773b7c0b2f
println <live.proto@hotmail.com>
2023-07-16 18:44:50 -0300

    fix film create route

--- src/app/@modules/domain/star-wars/film/film-routing.module.ts
+++ src/app/@modules/domain/star-wars/film/film-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
         },
       },
       {
-        path: PATHS.empty,
+        path: PATHS.create,
         pathMatch: 'full',
         component: FilmFormComponent,
       },
