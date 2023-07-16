139b543081a76ff999a1e01ed16f0c1c9a95b802
println <live.proto@hotmail.com>
2023-07-16 12:58:11 -0300

    fix jest(test) config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
-// https://github.com/thymikee/jest-preset-angular#brief-explanation-of-config
 module.exports = {
   preset: 'jest-preset-angular',
-  roots: ['src'],
-  coverageDirectory: 'reports',
   setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
   moduleNameMapper: {
     '@app/(.*)': '<rootDir>/src/app/$1',
@@ -15,18 +12,4 @@ module.exports = {
     '@env': '<rootDir>/src/environments/environment',
     '@translations': '<rootDir>/src/translations',
   },
-  transform: {
-    '^.+\\.(ts|js|html)$': 'ts-jest',
-  },
-  // globals: {
-  //   'ts-jest': {
-  //     allowSyntheticDefaultImports: true,
-  //     tsconfig: '<rootDir>/tsconfig.spec.json',
-  //     diagnostics: {
-  //       ignoreCodes: ['TS151001'],
-  //     },
-  //   },
-  // },
-  // Do not ignore librairies such as ionic, ionic-native or bootstrap to transform them during unit testing.
-  transformIgnorePatterns: ['node_modules/(?!@angular|@ngx-translate|@ng-bootstrap|@ngneat)'],
 };
